76c92a663c11f0c1b117faef57109a99af6c0cff
MaxSomov <sirbormotron@gmail.com>
2016-09-18 19:36:16 +0300

    tmp

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,10 +5,10 @@ return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 
-	'connectionString' => 'mysql:host=localhost;dbname=lozay',
+	'connectionString' => 'mysql:host=mysql87.1gb.ru;dbname=gb_lozay15',
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
+	'username' => 'gb_lozay15',
+	'password' => 'f7aa63e8a',
 	'charset' => 'utf8',
 
 );
